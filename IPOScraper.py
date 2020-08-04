9d6a49eb889c753d176accebcf015cb2fe03c18e
MNafekh <martinnafekh@hotmail.com>
2020-08-03 21:05:44 -0400

    added requirements.txt

--- IPOScraper.py
+++ IPOScraper.py
@@ -2,17 +2,37 @@ import discord
 import requests
 import os
 import json
-from lxml import etree
 from datetime import date
-from urllib.request import urlopen
 from requests_html import HTMLSession
-from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 from prettytable import PrettyTable
 
 load_dotenv()
 
 TOKEN = os.getenv('FINN_KEY')
+
+#don't know how to get future IPOs, deprecated for now
+def get_ipos() -> discord.Embed:
+    """
+    Returns a list of the IPOs taking place in the current month
+    Sample API output:
+    {'date': '2020-07-02', 'exchange': 'NYSE', 'name': 'Lemonade, Inc.', 'numberOfShares': 11000000, 'price': '29.00'
+        , 'status': 'priced', 'symbol': 'LMND', 'totalSharesValue': 319000000}
+    """
+    now = date.today()
+    fromdt = date(now.year, now.month, 1)
+    todt = date(now.year, now.month + 1, 1)
+    title = "Upcoming IPOs for {0:%B}".format(now)
+    description=""
+    url = 'https://finnhub.io/api/v1/calendar/ipo?from=' + str(fromdt) + '&to=' + str(todt) + '&token=' + TOKEN
+    print(url)
+    r = requests.get(url)
+    data = r.json()['ipoCalendar'] 
+    for t in data:
+        line = t['name'] + " | " + t['date'] + "\n"
+        description += line
+    return discord.Embed(title=title, description=description)
+
 
 def get_ipos_nasdank() -> discord.Embed:
     """
